a52f4dcf0684f19d3f44ce9996b6e46c990d8329
Franziska von der Goltz <franziska@vdgoltz.eu>
2017-04-30 09:42:02 -0700

    wrote initial Todo component that renders headline to DOM by react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log("Webpack is working");
+class ToDo extends React.Component {
+    render() {
+        return (
+                <h1>ToDo App </h1>
+            );
+    }
+}
 
+ReactDOM.render(
+    <ToDo />,
+    document.getElementById("app")
+    )
